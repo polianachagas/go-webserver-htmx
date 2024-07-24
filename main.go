f34fc05bb556d57fb79db76a322d5f0d0ee33a73
Poliana Chagas <polianarc934@gmail.com>
2024-07-24 20:44:49 -0300

    url do servidor e func handler

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("oi")
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+		io.WriteString(w, r.Method)
+	}
+	http.HandleFunc("/", h1)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
